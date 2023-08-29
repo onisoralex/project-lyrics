f1798af19a950093b73fe80d3a845b9505144766
Alexander Onisor <alexander.onisor@workflow.at>
2023-08-29 16:02:21 +0200

    Added docs

--- src/LyricsParsing/BasicInfoFunctions.js
+++ src/LyricsParsing/BasicInfoFunctions.js
@@ -1,5 +1,11 @@
 import * as Utils from "../Utilities/Utils.js";
 
+/**
+ * Searches for a keyword in a list of songparts and returns the index of that songpart of the array
+ * @param {*} parts 
+ * @param {*} keyWord 
+ * @returns 
+ */
 const getIndexOfPart = (parts, keyWord) => {
   for (let i = 0; i < parts.length; i++) {
     if (parts[i].name === keyWord) {
@@ -10,6 +16,12 @@ const getIndexOfPart = (parts, keyWord) => {
   return -1;
 };
 
+/**
+ * Checks if the info array contains a keyword fro the list of keywords and retrieves and returns the information from that line
+ * @param {*} basicSongInformationArray
+ * @param {*} keyWords
+ * @returns
+ */
 const getInfoFromLine = (basicSongInformationArray, keyWords) => {
   let info = "";
 
@@ -23,6 +35,13 @@ const getInfoFromLine = (basicSongInformationArray, keyWords) => {
   return info;
 };
 
+/**
+ * Checks if the basic information array contains a keyword from a list of keywords and if not returns an empty info object
+ * @param {*} keyWords
+ * @param {*} emptyInfo
+ * @param {*} basicSongInformationArray
+ * @returns
+ */
 const getBasicInfo = (keyWords, emptyInfo, basicSongInformationArray) => getInfoFromLine(basicSongInformationArray, keyWords) || emptyInfo;
 
 export {
